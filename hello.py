1aaecb47d844249b83916e06748c32c363aa9fd7
Luis <Lmx154>
2024-09-30 18:47:56 -0500

    added venv

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt
+
 print("hi")
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+plt.plot(x, y, marker='o', markerfacecolor='blue', linestyle='-', linewidth=2, color='blue')
+plt.title('Basic Line Graph of x squared')
+plt.xlabel('x value')
+plt.ylabel('x squared')
+plt.show()
